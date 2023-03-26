ebfc9951918850c6cefe9c45d165745413aad19f
rectangulared <rectangulared@gmail.com>
2023-03-26 20:50:19 +0300

    Implemented basic abstraction for renderbuffer

---
+++ ProjectPoint/render/entities/renderbuffer/Renderbuffer.cpp
@@ -0,0 +1,33 @@
+#include "Renderbuffer.h"
+
+Renderbuffer::Renderbuffer(const GLenum& internalFormat, const GLenum& attachment,const GLuint& width, const GLuint& height)
+{
+	this->_rbWidth = width;
+	this->_rbHeight = height;
+
+	glGenRenderbuffers(1, &_id);
+	glBindRenderbuffer(GL_RENDERBUFFER, _id);
+	glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, attachment, GL_RENDERBUFFER, _id);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
+
+Renderbuffer::~Renderbuffer()
+{
+	this->destroy();
+}
+
+void Renderbuffer::bind()
+{
+	glBindRenderbuffer(GL_RENDERBUFFER, _id);
+}
+
+void Renderbuffer::unbind()
+{
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
+
+void Renderbuffer::destroy()
+{
+	glDeleteRenderbuffers(1, &_id);
+}
---
+++ ProjectPoint/render/entities/renderbuffer/Renderbuffer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "glad/glad.h"
+
+class Renderbuffer
+{
+private:
+	GLuint _id;
+	GLuint _rbWidth;
+	GLuint _rbHeight;
+
+public:
+	Renderbuffer(const GLenum& internalFormat, const GLenum& attachment, const GLuint& width, const GLuint& height);
+
+	~Renderbuffer();
+
+	void bind();
+
+	void unbind();
+
+	void destroy();
+};
